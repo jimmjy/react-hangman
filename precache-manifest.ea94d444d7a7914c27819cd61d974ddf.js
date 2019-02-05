b6c2431cd5cd3da8f4ceda4039db3df208e41b51
James Finkelstein <jamesfinkelstein@Jamess-MacBook.local>
2019-02-05 09:22:02 -0500

    Updates

---
+++ precache-manifest.ea94d444d7a7914c27819cd61d974ddf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d69552ac1fc5b0099d",
+    "url": "/react-hangman/static/js/runtime~main.c0d69552.js"
+  },
+  {
+    "revision": "933fc6fefc3c3a72b730",
+    "url": "/react-hangman/static/js/main.933fc6fe.chunk.js"
+  },
+  {
+    "revision": "db2991ed0d4350328408",
+    "url": "/react-hangman/static/js/1.db2991ed.chunk.js"
+  },
+  {
+    "revision": "933fc6fefc3c3a72b730",
+    "url": "/react-hangman/static/css/main.f4d7fb2c.chunk.css"
+  },
+  {
+    "revision": "f5ea92a2928d85b1f88667514ccd533e",
+    "url": "/react-hangman/index.html"
+  }
+];
